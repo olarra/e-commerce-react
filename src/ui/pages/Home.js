24557c8da2a7746fb870177c508919f5423e3329
Jesus <horus_hades@hotmail.com>
2019-08-06 23:46:28 +0200

    feat(ui): home style finish

--- src/ui/pages/Home.js
+++ src/ui/pages/Home.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 // import Main from "../components/Main";
 // import SideBar from "../components/SideBar";
-import { Container, Row, Col, Card, Pagination, Button } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  ButtonGroup,
+  Button,
+  Badge,
+  ButtonToolbar
+} from "react-bootstrap";
 import { NavBar, ArticleCard } from "../common";
 
 export class Home extends Component {
@@ -42,15 +50,42 @@ export class Home extends Component {
       <div className="c-home">
         <NavBar />
         <Container>
-          <div>page: {pagination.page}</div>
-          <div>size: {pagination.size}</div>
-          <select name="size" id="size" onChange={e => setSize(e)}>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="25">25</option>
-          </select>
-          <button onClick={previousPage}>Previous Page</button>
-          <button onClick={nextPage}>Next Page</button>
+          <Row
+            style={{
+              marginTop: "40px",
+              marginLeft: "5px",
+              marginBottom: "40px"
+            }}
+          >
+            <Col>
+              <ButtonToolbar aria-label="Toolbar with button groups">
+                <ButtonGroup className="mr-2" aria-label="size">
+                  <select name="size" id="size" onChange={e => setSize(e)}>
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="25">25</option>
+                  </select>
+                </ButtonGroup>
+
+                <ButtonGroup className="mr-2" aria-label="Second group">
+                  <Button onClick={previousPage}>Previous Page</Button>
+                </ButtonGroup>
+
+                <ButtonGroup aria-label="Third group">
+                  <Button onClick={nextPage}>Next Page</Button>
+                </ButtonGroup>
+              </ButtonToolbar>
+            </Col>
+            <Col style={{ display: "flex" }}>
+              <h5>
+                Page: <Badge variant="secondary">{pagination.page}</Badge>
+              </h5>
+              <h5 style={{ marginLeft: "15px" }}>
+                Size: <Badge variant="secondary">{pagination.size}</Badge>
+              </h5>
+            </Col>
+          </Row>
+
           {pagination.currentPage
             ? this.renderList(pagination)
             : this.renderEmpty()}
