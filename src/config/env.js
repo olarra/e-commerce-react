de4455b91737829f4a1f43775626a4ea587d3e5b
Jesus <horus_hades@hotmail.com>
2019-08-07 22:33:22 +0200

    fix(env): usless code

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,3 @@
 export default {
-  API_ENDPOINT_PRODUCTS: "https://jsonplaceholder.typicode.com/photos",
-  API_ENDPOINT_SHOP: "./data/shop.json"
+  API_ENDPOINT_PRODUCTS: "https://jsonplaceholder.typicode.com/photos"
 };
